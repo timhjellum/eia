2c59385fc3fad6bade6387518b16e97f155a1d8a
timoteo <tim@edelweissco.com>
2018-01-13 15:59:06 -0800

    Synched

--- style-guide/app/assets/scripts/modules/_header-menu.js
+++ style-guide/app/assets/scripts/modules/_header-menu.js
@@ -1,6 +1,6 @@
-	//import $ from 'jquery';
-	import fancybox from '../../../../node_modules/fancybox/dist/js/jquery.fancybox.js';
-	import fancyboxPack from '../../../../node_modules/fancybox/dist/js/jquery.fancybox.pack.js';
+//import $ from 'jquery';
+import fancybox from '../../../../node_modules/fancybox/dist/js/jquery.fancybox.js';
+import fancyboxPack from '../../../../node_modules/fancybox/dist/js/jquery.fancybox.pack.js';
 
 
 class HeaderMenu {
@@ -15,68 +15,47 @@ class HeaderMenu {
 		this.checkSize();
 		this.events();
 	}
-	// check the page resolution on initial page load and if the browser is resized
-	//	$(document).ready(function() {
-	// run on initial page load
-	//checkSize();
-	// run on window resize
-	//	$(window).resize(checkSize);
-
 	events() {
-		
-		//this.window.resize(checkSize);
 		this.window.resize(this.checkSize.bind(this));
 		this.menuItem.click(this.openMenu.bind(this));
-		//var btb = document.getElementByClass('fancybox-menu');
-		
-		//btn.addEventListenerByClass('click', function () {
-		
-		//$('.fancybox-menu').click(function () {
-
-		//});
 	}
-	//	});
-
-openMenu() {
-	console.log("clicked");
-	var target = $(this).data("target");
-	var options = {active : 0};
-	switch(target) {
-		case 'sources':
-		options.active = 0;
-		break;
-		case 'topics':
-		options.active = 1;
-		break;
-		case 'geography':
-		options.active = 2;
-		break;
-		case 'tools':
-		options.active = 3;
-		break;
-		case 'learn':
-		options.active = 4;
-		break;
-		case 'news':
-		options.active = 5;
-		break;
-		case 'default':
-		options.active = 0;
+	openMenu() {
+		console.log("clicked");
+		var target = $(this).data("target");
+		var options = {active : 0};
+		switch(target) {
+			case 'sources':
+			options.active = 0;
+			break;
+			case 'topics':
+			options.active = 1;
+			break;
+			case 'geography':
+			options.active = 2;
+			break;
+			case 'tools':
+			options.active = 3;
+			break;
+			case 'learn':
+			options.active = 4;
+			break;
+			case 'news':
+			options.active = 5;
+			break;
+			case 'default':
+			options.active = 0;
+		}
+		$('.section-tabs').tabs(options);
 	}
-	$('.section-tabs').tabs(options);
-}
 
-
-
-
-checkSize() {
-	console.log('Browser window opened or resized!');
-	this.searchPanel.removeClass('show');
-	this.header.removeClass('show');
-	this.logo.removeClass('show');	
-	//	$('.search-panel').removeClass('show');
-	//	$('header').removeClass('show');
-	//	$('.logo').removeClass('show');
+	checkSize() {
+		console.log('Browser window opened or resized!');
+		this.searchPanel.removeClass('show');
+		this.header.removeClass('show');
+		this.logo.removeClass('show');	
+		//	$('.search-panel').removeClass('show');
+		//	$('header').removeClass('show');
+		//	$('.logo').removeClass('show');
 
 		// outer wrapper is set as 990px for laptop and desktop
 		var layoutWrapperWidth = $("footer").width();
