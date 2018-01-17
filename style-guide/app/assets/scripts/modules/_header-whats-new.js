308b61e23012c2f7da7f18850044895e239c1572
timhjellum <timhjellum@gmail.com>
2018-01-16 20:40:11 -0500

    Synched

--- style-guide/app/assets/scripts/modules/_header-whats-new.js
+++ style-guide/app/assets/scripts/modules/_header-whats-new.js
@@ -4,14 +4,7 @@ console.log('whats new checking in...');
 
 class HeaderWhatsNew {
     constructor() {
-        /*
-        this.element = $('#whatsnew3');
-        this.load();
-    }
-    load() {
-        this.element().load('https://www.eia.gov/CFusionMX7/global/data/whatsnew.htm');
-        */
-        $('#whatsnew3').load('https://www.eia.gov/CFusionMX7/global/data/whatsnew.htm');
+        $('.header-whats-new').load('/global/css_rehab_eia-header-whats-new.inc');
     }
 }
 export default HeaderWhatsNew;
