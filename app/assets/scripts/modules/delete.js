1b889a3c4d2fd0f7804c418a409b7752dcb5c22d
Tim <timhjellum@gmail.com>
2018-10-16 10:40:13 -0700

    sync

--- app/assets/scripts/modules/delete.js
+++ app/assets/scripts/modules/delete.js
@@ -1,8 +1,11 @@
-
-
-
-
-
-
-
-
+<
+/li> <
+li class = "_nav-primary-item" >
+    <
+    ul class = "_themes _nav-dropdown" > < /ul> < /
+li > <
+    li class = "_nav-primary-item" >
+    <
+    ul class = "_components _nav-dropdown" > < /ul> < /
+li > <
+    /ul>
--- app/assets/scripts/modules/style-guide/_header.js
+++ app/assets/scripts/modules/style-guide/_header.js
@@ -11,121 +11,45 @@ class Header {
             debounce: false
         })
 
+        $("._base").html('<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/color-palette.html">Color Palette</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/forms.html">Forms</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/icons.html">Icons</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/lists.html">Lists</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/size-conversions.html">Size Conversions</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/symbols.html">Symbols</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/tables.html">Tables</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/base/typography.html"></a>Typography</li>');
 
-        $("._header").html('<a class="logo-white-background" href="/">' +
-            '<h1>U.S. Energy Information Administration - EIA - Independent Statistics and Analysis</h1>' +
-            '</a>' +
-            '<ul class="_nav">' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../base/">Base</a>' +
-            '       <ul class="_nav-dropdown">' +
-            '           <li><a href="../base/color-palette.html">Color Palette</a></li>' +
-            '           <li><a href="../base/forms.html">Forms</a></li>' +
-            '           <li><a href="../base/icons.html">Icons</a></li>' +
-            '           <li><a href="../base/lists.html">Lists</a></li>' +
-            '           <li><a href="../base/size-conversions.html">Size Conversions</li>' +
-            '           <li><a href="../base/symbols.html">Symbols</a></li>' +
-            '           <li><a href="../base/tables.html">Tables</a></li>' +
-            '           <li><a href="../base/typography.html">Typography</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../layouts/">Layouts</a>' +
-            '   </li>' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../modules/">Modules</a>' +
-            '       <ul class=""_nav-dropdown">' +
-            '           <li><a href="../modules/accordion.html">Accordions</a></li>' +
-            '           <li><a href="../modules/content-containers-home.html">Content Containers (home)</a></li>' +
-            '           <li><a href="../modules/content-containers-non-reusable.html">Content Containers (non-reusable)</a></li>' +
-            '           <li><a href="../modules/content-containers-reusable.html">Content Containers (reusable)</a></li>' +
-            '           <li><a href="../modules/fancybox.html">Modals (FancyBox)</a></li>' +
-            '           <li><a href="../modules/lists.html">Lists</a></li>' +
-            '           <li>Messaging</li>' +
-            '           <li><a href="../modules/page-titles.html">Page Titles</a></li>' +
-            '           <li><a href="../modules/table-list-formatting.html">Tables</a></li>' +
-            '           <li><a href="../modules/tabs.html">Tabs</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../states/">States</a>' +
-            '       <ul class=""_nav-dropdown">' +
-            '           <li><a href="empty-list-items.html">Empty List Items</a></li>' +
-            '           <li>sticky-table-headers.html</li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../themes/">Themes</a>' +
-            //            '       <ul class=""_nav-dropdown">' +
-            //            '           <li><a href=""></a></li>' +
-            //            '       </ul>' +
-            '   </li>' +
-            '   <li class="_nav-primary-item">' +
-            '       <a href="../components/">Components</a>' +
-            '       <ul class=""_nav-dropdown">' +
-            '           <li>Sliders</li>' +
-            '           <li><a href="../modules/sub-navigation.html">Navigation</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '</ul>' +
-            '<div class="_resolution _mobile">Mobile</div>' +
-            '<div class="_resolution _mobile-landscape">Mobile Landscape</div>' +
-            '<div class="_resolution _tablet">Tablet</div>' +
-            '<div class="_resolution _tablet-landscape">Tablet Landscape</div>' +
-            '<div class="_resolution _laptop">Laptop +</div>');
+        $("._layouts").html('');
 
-        $("._footer").html('<a class="logo-white-background" href="/">' +
-            '<h1>U.S. Energy Information Administration - EIA - Independent Statistics and Analysis</h1>' +
-            '</a>' +
-            '<ul>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../base/>Base</a></strong></li>' +
-            '           <li><a href="../base/color-palette.html">Color Palette</a></li>' +
-            '           <li><a href="../base/icons.html">Icons</a></li>' +
-            '           <li><a href="../base/typography.html">base/typography.html</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../layouts/>Layouts</a></strong></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../modules/">Modules</a></strong></li>' +
-            '           <li><a href="../modules/accordion.html">Accordions</a></li>' +
-            '           <li><a href="../modules/content-containers-home.html">Content Containers (home)</a></li>' +
-            '           <li><a href="../modules/content-containers-non-reusable.html">Content Containers (non-reusable)</a></li>' +
-            '           <li><a href="../modules/content-containers-reusable.html">Content Containers (reusable)</a></li>' +
-            '           <li><a href="../modules/fancybox.html">Modals (FancyBox)</a></li>' +
-            '           <li><a href="../modules/lists.html">Lists</a></li>' +
-            '           <li>Messaging</li>' +
-            '           <li><a href="../modules/page-titles.html">Page Titles</a></li>' +
-            '           <li><a href="../modules/table-list-formatting.html">Tables</a></li>' +
-            '           <li><a href="../modules/tabs.html">Tabs</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../states/">States</a></strong></li>' +
-            '           <li><a href="empty-list-items.html">Empty List Items</a></li>' +
-            '           <li>sticky-table-headers.html</li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../themes/">Themes</a></strong></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '   <li>' +
-            '       <ul>' +
-            '           <li><strong><a href="../components/">Components</a></strong></li>' +
-            '           <li>Sliders</li>' +
-            '           <li><a href="../modules/sub-navigation.html">Navigation</a></li>' +
-            '       </ul>' +
-            '   </li>' +
-            '</ul>');
+        $("._modules").html('<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/accordion.html">Accordions</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/content-containers-home.html">Content Containers (home)</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/content-containers-non-reusable.html">Content Containers (non-reusable)</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/content-containers-reusable.html">Content Containers (reusable)</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/fancybox.html">Modals (FancyBox)</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/lists.html">Lists</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/page-titles.html"></a>Messaging</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/page-titles.html">Page Titles</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/table-list-formatting.html">Tables</a></li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/tabs.html"></a>Tabs</li>');
+
+        $("._states").html('<li><a href="empty-list-items.html"></a>Empty List Items</li>' +
+            '<li>sticky-table-headers.html</li>');
+
+        $("._themes").html('<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/accordion"></a>Accordion</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/basic-table"></a>Basic Tables</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/data"></a>Date Tables</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/natural-gas"></a>Natural Gas</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/overview"></a>Overview</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/primary-col-modules"></a>Primary Modules</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/theme/secondary-col-modules"></a>Secondary Modules</li>');
+
+        $("._components").html('<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/sub-navigation.html"></a>Footer</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/sub-navigation.html"></a>Header</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/sub-navigation.html"></a>Home Slider</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/sub-navigation.html"></a>L2 Sliders</li>' +
+            '<li><a href="https://www.eia.gov/css_rehab/archive/style-guide/app/modules/sub-navigation.html"></a>Sub-navigation</li>');
+
 
 
         //if ($('.navbar-brand:contains(Base)').length > 0) {
@@ -264,86 +188,7 @@ class Header {
             $(".eia-light-white-50").html($(".eia-light-white-50").css("backgroundColor"));
             $(".eia-light-white-75").html($(".eia-light-white-75").css("backgroundColor"));
             $(".eia-light-white").html($(".eia-light-white").css("backgroundColor"));
-
-
         });
-
-
-
-
-
-
-
-
-
-
-        var dallyOne = $('.dally-fou-one span._module-bg._col-fou-one-one').width();
-        console.log(dallyOne);
-        $('.ding').append(dallyOne);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
 export default Header;
-
-
-
-/*
-<li><a href="../base/color-palette.html">Color Palette</a></li>
-<li><a href="../base/header-and-footer.html" target="_blank">Header & Footer</a></li>
-<li><a href="../base/icons.html">Icons</a></li>
-<li><a href="../base/typography.html">Typography</a></li>
-
-
-<li><a href="../layouts/index.html#header-row">Header Row</a></li>
-<li><a href="../layouts/index.html#row">Row</a></li>
-<li><a href="../layouts/index.html#full-width">Full-width</a></li>
-<li><a href="../layouts/index.html#side+main+side">Side + Main + Side</a></li>
-<li><a href="../layouts/index.html#main+side">Main + Side</a></li>
-<li><a href="../layouts/index.html#main+wide-side">Main + Wide-side</a></li>
-<li><a href="../layouts/index.html#wide-side+main">Wide-side + Main</a></li>
-<li><a href="../layouts/index.html#one-third+two-thirds">One-third + Two-thirds</a></li>
-<li><a href="../layouts/index.html#two-thirds+one-third">Two-thirds + One-third</a></li>
-<li><a href="../layouts/index.html#two-even-columns">Two-even-columns</a></li>
-<li><a href="../layouts/index.html#three-even-columns">Three-even-columns</a></li>
-<li><a href="../layouts/index.html#four-even-columns">Four-even-columns</a></li>
-
-
-<li><a href="../modules/accordion.html">Accordion</a></li>
-<li><a href="../modules/home-content-containers.html">Home content Containers</a></li>
-<li><a href="../modules/reusable-content-containers.html#standard-containers">Standard Containers</a></li>
-<li><a href="../modules/reusable-content-containers.html#light-blue-top-border">Light-blue-top-border Containers</a></li>
-<li><a href="../modules/reusable-content-containers.html#light-blue-top-border-dilly">Light-blue-top-border dilly Containers</a></li>
-<li><a href="../modules/reusable-content-containers.html#dark-blue-top-border">Dark-blue-top-border Containers</a></li>
-<li><a href="../modules/report-header.html">Report Header</a></li>
-<li><a href="../modules/sub-navigation.html">Sub Navigation</a></li>
-<li><a href="../modules/table-list-formatting.html">Table and List Formatting</a></li>
-<li><a href="../modules/tabs.html">Tabs</a></li>
-<li><a href="../modules/page-titles.html">Page Titles</a></li>
-<li><a href="../modules/messaging.html">Messaging</a></li>
-<li><a href="../modules/side-content-containers.html">Side Content Containers</a></li>
-<li><a href="../modules/slider-home.html" target="_blank">Home Slider</a></li>
-<li><a href="../modules/slider-multi-cards.html" target="_blank">Multi-Card Slider</a></li>
-
-
-<li><a href="../states/links-list.html">Links List</a></li>
-<li><a href="../states/header-menu.html">Header Menus</a></li>
-<li><a href="../states/sub-nav-menus.html">Subnav Menus</a></li>
-<li><a href="../states/local-scroll.html">Local Scroll</a></li>
-<li><a href="../states/scroll-to.html">Scroll To</a></li>
-
-*/
