a984e368690b1052c5da3baeb740baaa18a94bf2
thm <timothy.hjellum@eia.gov>
2020-09-10 14:00:12 -0400

    latest version

--- src/components/ModulesFootnotes.js
+++ src/components/ModulesFootnotes.js
@@ -1,20 +1,20 @@
 ﻿import React, { Component } from "react"
 import Page from "./Page"
 
-import TablesFootnotesExample from "./tables/TablesFootnotesExample"
+import TablesFootnotesExample from "./Tables/TablesFootnotesExample"
 import Footnotes from "./modules/Footnotes"
 
 class ModulesFootnotes extends Component {
-	render() {
-		return (
-			<Page title="Footnotes">
-				<span className="modules">
-					<h1 className="_h1">Footnotes</h1>
-					<TablesFootnotesExample />
-					<Footnotes />
-				</span>
-			</Page>
-		)
-	}
+  render() {
+    return (
+      <Page title="Footnotes">
+        <span className="modules">
+          <h1 className="_h1">Footnotes</h1>
+          <TablesFootnotesExample />
+          <Footnotes />
+        </span>
+      </Page>
+    )
+  }
 }
 export default ModulesFootnotes
